e3ef0c3d20214eaddc134967e9969f4c2b5be3a0
praharshapm <69325690+praharshapm@users.noreply.github.com>
2020-08-11 00:00:46 +0530

    Update AMUX2_3V.v

--- verilog/AMUX2_3V.v
+++ verilog/AMUX2_3V.v
@@ -1,10 +1,4 @@
-// Analog 2-input, 1-output multiplexer
-// with 1.8V digital select.
-//
-// NOTE:  This is a simple functional model only and captures
-// none of the internal workings of the multiplexer, such
-// as double-gating each input and connecting the middle node
-// to VSSA on the unselected input.
+
 
 module AMUX2_3V (
    input VDD, 
